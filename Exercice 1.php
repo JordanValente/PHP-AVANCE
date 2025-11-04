683ac075adc4e457b4f51fee69193e581b041396
Jordan Valente <jordanvalente14@gmail.com>
2025-11-04 14:26:31 +0100

    exo3

--- Exercice 1.php
+++ Exercice 1.php
@@ -16,6 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 */
 ?>
+<!--EX1-->
 
 <?php
 $annenaissance = 1904;
@@ -51,6 +52,7 @@ foreach ($years as $value) {
     echo $value."<br>";
 }
 ?>
+<!--EX2-->
 
 <?php
 echo "<br>";
@@ -70,5 +72,70 @@ foreach ($annees_naissance as $annee) {
 echo "année la plus grande : <br>".$annee_max."<br>";
 echo "année la plus petite : <br>".$annee_min."<br>";
 echo "nombre d'année paires : <br>".$parite."<br>";
+echo "<br>";
+echo "====================================================";
+echo "<br>";
 ?>
+<!--EX3-->
 
+<?php
+// Définition de la classe Personne
+class Personne {
+    // Attributs
+    public $nomex2;
+    public $prenomex2;
+    public $ageex2;
+
+    // Méthode pour afficher l'identité
+    public function monIdentite() {
+        echo "Nom : " . $this->nomex2 . "<br>";
+        echo "Prénom : " . $this->prenomex2 . "<br>";
+        echo "Âge : " . $this->ageex2 . " ans<br>";
+    }
+    // Méthode pour afficher uniquement le nom
+    public function afficheMonNom() {
+        return "Mon nom est " . $this->nomex2;
+    }
+}
+
+// Création d'un objet Personne
+$personne1 = new Personne();
+$personne1->nomex2 = "Valente";
+$personne1->prenomex2 = "Jordan";
+$personne1->ageex2 = 18;
+
+
+// Appel de la méthode
+$personne1->monIdentite(); echo "<br>";
+echo $personne1->afficheMonNom();
+echo "<br>";
+echo "====================================================";
+echo "<br>";
+?>
+<!--EX3 Voiture -->
+<?php
+class Voiture {
+    public $marque;
+    public $modele;
+    public $anneeDeFabrication;
+    public $couleur;
+
+
+    public function afficher() {
+        echo "Marque : " . $this->marque . "<br>";
+        echo "Anné de fabrication : " . $this->anneeDeFabrication . "<br>";
+        echo "Couleur : " . $this->couleur . "<br>";
+        echo "modele : " . $this->modele . "<br>";
+    }
+}
+
+
+$maVoiture = new Voiture();
+$maVoiture->marque = "Renault";
+$maVoiture->modele = "clio4rs";
+$maVoiture->anneeDeFabrication = 2013;
+$maVoiture->couleur = "Rouge et blanche";
+
+
+$maVoiture->afficher();
+?>
